b522f5de5d2d819a0652dbc2a7c806bffb325671
Hylke Visser <hylke@hylkev.com>
2011-11-25 16:00:00 +0100

    Modified main controller
    Execute an example query on the Million Song dataset

--- www/application/classes/controller/welcome.php
+++ www/application/classes/controller/welcome.php
@@ -4,7 +4,23 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
-		$this->response->body('hello, world!');
-	}
+            $database = Database::instance();
+            
+            //load Million Song Dataset files
+            $database->attach('artist_term','lastfm_similars','lastfm_tags','track_metadata');
+            
+            $r = $database->query(Database::SELECT, "
+                SELECT * FROM (
+SELECT artist_id, count(*) AS count FROM artist_term
+WHERE artist_term.term = \"jazz\"
+OR artist_term.term = \"80s\"
+OR artist_term.term = \"90s\"
+GROUP BY artist_id
+) ORDER BY count DESC
+LIMIT 1000");
+            
+            $this->response->body("<pre>" . Debug::dump($r) . "</pre>");
+            
+        }
 
 } // End Welcome
