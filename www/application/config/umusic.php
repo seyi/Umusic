93a73d9c6617d40f82eebc010a735673f2c4a2c5
Hylke Visser <hylke@hylkev.com>
2011-11-25 16:00:10 +0100

    UMusic configuration

---
+++ www/application/config/umusic.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('SYSPATH') or die('No direct access allowed.');
+
+return array
+    (
+    'dataset' => array(
+        'dir' => '/umusic_data/',
+        'ext' => 'sqlite',
+    ),
+    'echonest' => array(
+        'api_key'       => '4M0DFUG9R81A8U7OB',
+        'consumer_key'  => '7182fa50a219e737b3fc0f445a38ae76',
+        'shared_secret' => 'lpK8ZhjoSSiUfwoBJGzLUQ',
+    ),
+    'lastfm' => array(
+        'api_key'   =>  'cb4d0e82fd5feaceced5dcd045034065',
+        'secret'    =>  '8fae45ccd2d93b336f2ea59ebcf97883',
+    )
+);
