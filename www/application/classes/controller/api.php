0a2a4c2f72bcde85f7ac113270b39767070fe075
Hylke Visser <hylke@hylkev.com>
2011-12-16 17:15:55 +0100

    API modifications

--- www/application/classes/controller/api.php
+++ www/application/classes/controller/api.php
@@ -14,25 +14,43 @@ defined('SYSPATH') or die('No direct script access.');
 class Controller_Api extends Controller {
 
     private $result;
+    private $render = true;
 
     public function before() {
         parent::before();
         $this->database = Database::instance();
         $this->database->attach('artist_term', 'lastfm_similars', 'lastfm_tags', 'track_metadata');
         $this->response->headers("Content-type", "text/plain");
-        $this->result = array(
-            'api-version'   => '0.1',
-        );
+        $this->result = array();
     }
     
     public function action_index() {
         $this->result = Arr::merge($this->result, array(
             'status' => 0,
+            'message' => 'Welcome',
         ));
     }
     
+    public function action_log() {
+        if($this->request->method() == 'POST') {
+            $log = json_decode($this->request->get('log'));
+            print_r($log);
+        } else {
+            $this->error('This method requires POST data');
+        }
+    }
+    
+    private function error($message,$code=1) {
+        echo json_encode(array(
+            'status' => $code,
+            'message' => $message,
+        ));
+        $this->render = false;
+    }
+    
     public function after() {
-        echo json_encode($this->result);
+        if($this->render)
+            echo json_encode($this->result);
         parent::after();
     }
 
