84070f0b3c6932cc16c614a6198451d43272be34
bottervanger <berendottervanger@gmail.com>
2012-01-18 14:40:20 +0100

    recommendation comparing should be static

--- www/application/classes/umusic/Umusic_Eval.php
+++ www/application/classes/umusic/Umusic_Eval.php
@@ -13,7 +13,7 @@ class Umusic_Eval{
 	*@param min the bare minimum of our calculated similarity for which we consider two tracks similar.
 	*@return void this function will no longer return anything, rather it writes a file containing the results of the comparison.
 	*/
-	public function compare_recommendations($number = 1, $min = 0.8) //for one song only for now since it's quite an extensive calculation.
+	public static function compare_recommendations($number = 1, $min = 0.8) //for one song only for now since it's quite an extensive calculation.
 	{
 		$Checked = array();
 		$comparisons = 0;
